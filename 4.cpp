0c18780d10cbf0bdf33d373201b81de22e3a460c
ANKIT RAJ <ankitraj311@users.noreply.github.com>
2020-09-22 23:27:23 +0530

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -9,22 +9,24 @@ int main()
     int N;
     cin >> N;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    if(N%2 != 0) //Mycode ******
+   if(N%2 == 0) //Mycode ******
     {
-       cout <<"Weird";
+      if(N>=2 && N<=5)
+      { //Mycode ******
+      cout<<"Not Weird";
+      }
+      else if(N>=6 && N<=20) //Mycode ******
+      {
+       cout<<"Weird";
+      }
+      else //Mycode ******
+     { 
+       cout<<"Not Weird";
+     }
     }
-    else if(2<N<5) //Mycode ******
+    else 
     {
-      cout <<"Not Weird";
+      cout<<"Weird";
     }
-    else if(6<N<20) //Mycode ******
-    {
-     cout <<"Weird";
-    }
-    else //Mycode ******
-    {
-     cout <<"Not Weird";
-    }
-
     return 0;
 }
